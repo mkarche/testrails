4de9c72955685840b7a15781eeeb38be829e855e
mkarche <karchemohamed@gmail.com>
2021-05-10 21:43:51 +0000

    adding Product creation feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,25 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find_by(id: params["id"])
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new (product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name,:tagline,:category)
+  end
+
 end
