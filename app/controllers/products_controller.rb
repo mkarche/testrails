f0455eaee74363dc12ded28d448080b5aebd5477
mkarche <karchemohamed@gmail.com>
2021-05-11 09:04:18 +0000

    adding features update & delete product + refacto/optimizing code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :product_find, only: [:show, :edit, :update, :destroy]
   def index
     if params["filter"]
       @products = Product.where(category: params["filter"])
@@ -7,13 +8,11 @@ class ProductsController < ApplicationController
     end
   end
   def show
-    @product = Product.find_by(id: params["id"])
+    #@product = Product.find_by(id: params["id"])
   end
-
   def new
     @product = Product.new
   end
-
   def create
     @product = Product.new (product_params)
     if @product.save
@@ -21,10 +20,29 @@ class ProductsController < ApplicationController
     else
       render :new
     end
-
+  end
+  def edit
+    #@product = Product.find_by(id: params[:id])
+  end
+  def update
+    #@product = Product.find_by(id: params[:id])
+    if @product.update (product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+  def destroy
+    #@product = Product.find_by(id: params[:id])
+    @product.destroy
+    redirect_to products_path
   end
 
   private
+
+  def product_find
+    @product = Product.find_by(id: params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:name,:tagline,:category)
